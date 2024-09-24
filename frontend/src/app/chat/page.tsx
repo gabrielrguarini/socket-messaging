98059d1f5b221ced6010ce091e0df70cdfafbe56
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-09-24 20:28:42 -0300

    feat: add sheet for mobile

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,11 +1,19 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useSearchParams } from "next/navigation";
 import socket from "@/socket";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 
 interface Message {
     id: number;
@@ -27,6 +35,8 @@ export default function Home() {
     const [messages, setMessages] = useState<Message[]>([]);
     const [users, setUsers] = useState<User[]>([]);
     const [inputMessage, setInputMessage] = useState("");
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const messagesEndRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if (username) {
@@ -58,6 +68,9 @@ export default function Home() {
             };
         }
     }, [username]);
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
 
     if (username === null) return null;
 
@@ -75,57 +88,78 @@ export default function Home() {
         }
     };
 
+    const Sidebar = () => (
+        <div className="w-full bg-white">
+            <ScrollArea className="h-[calc(100vh-5rem)]">
+                {users.map((user) => (
+                    <div
+                        key={user.id}
+                        className="flex items-center p-4 hover:bg-gray-100"
+                    >
+                        <Avatar className="h-10 w-10">
+                            <AvatarImage src={user.avatar} alt={user.name} />
+                            <AvatarFallback>{user.name[0]}</AvatarFallback>
+                        </Avatar>
+                        <div className="ml-4">
+                            <p className="text-sm font-medium">{user.name}</p>
+                            <p
+                                className={`text-xs ${
+                                    user.status === "online"
+                                        ? "text-green-500"
+                                        : "text-gray-500"
+                                }`}
+                            >
+                                {user.status}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </ScrollArea>
+        </div>
+    );
+
     return (
         <div className="flex h-screen bg-gray-100">
-            <div className="w-1/4 bg-white border-r">
-                <div className="p-4 border-b">
-                    <h2 className="text-xl font-semibold">Pessoas</h2>
-                </div>
-                <ScrollArea className="h-[calc(100vh-5rem)]">
-                    {users?.map((user) => (
-                        <div
-                            key={user.id}
-                            className="flex items-center p-4 hover:bg-gray-100"
-                        >
-                            <Avatar className="h-10 w-10">
-                                <AvatarImage
-                                    src={user.avatar}
-                                    alt={user.name}
-                                />
-                                <AvatarFallback>
-                                    {user?.name?.length > 0
-                                        ? user.name[0].toUpperCase()
-                                        : "?"}
-                                </AvatarFallback>
-                            </Avatar>
-                            <div className="ml-4">
-                                <p className="text-sm font-medium">
-                                    {user.name}
-                                </p>
-                                <p
-                                    className={`text-xs ${
-                                        user.status === "online"
-                                            ? "text-green-500"
-                                            : "text-gray-500"
-                                    }`}
-                                >
-                                    {user.status}
-                                </p>
-                            </div>
-                        </div>
-                    ))}
-                </ScrollArea>
+            <div className="hidden md:block md:w-1/4 bg-white border-r">
+                <h2 className="px-4 pt-4 text-xl font-semibold">Usuários</h2>
+                <p className="px-4 text-sm text-gray-400">
+                    Lista de usuários conectados no chat.
+                </p>
+                <Sidebar />
             </div>
             <div className="flex-1 flex flex-col">
-                <div className="p-4 border-b bg-white">
+                <div className="p-4 border-b bg-white flex justify-between items-center">
                     <h2 className="text-xl font-semibold">Chat</h2>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button
+                                variant="outline"
+                                size="icon"
+                                className="md:hidden"
+                            >
+                                <Menu className="h-4 w-4" />
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent
+                            side="left"
+                            className="w-[300px] sm:w-[400px]"
+                        >
+                            <SheetTitle>Usuários</SheetTitle>
+                            <SheetDescription>
+                                Lista de usuários conectados no chat.
+                            </SheetDescription>
+                            <Sidebar />
+                        </SheetContent>
+                    </Sheet>
                 </div>
                 <ScrollArea className="flex-1 p-4">
                     {messages.map((message) => (
                         <div key={message.id} className="mb-4">
                             <div className="flex items-start">
                                 <Avatar className="h-8 w-8 mr-2">
-                                    <AvatarFallback></AvatarFallback>
+                                    <AvatarFallback>
+                                        {message.sender[0]}
+                                    </AvatarFallback>
                                 </Avatar>
                                 <div>
                                     <p className="text-sm font-medium">
@@ -139,16 +173,17 @@ export default function Home() {
                             </p>
                         </div>
                     ))}
+                    <div ref={messagesEndRef} />
                 </ScrollArea>
                 <div className="p-4 bg-white border-t">
                     <div className="flex space-x-2">
                         <Input
-                            placeholder="Digite sua mensagem..."
+                            placeholder="Type a message..."
                             value={inputMessage}
                             onChange={(e) => setInputMessage(e.target.value)}
                             onKeyUp={(e) => e.key === "Enter" && sendMessage()}
                         />
-                        <Button onClick={sendMessage}>Enviar</Button>
+                        <Button onClick={sendMessage}>Send</Button>
                     </div>
                 </div>
             </div>
